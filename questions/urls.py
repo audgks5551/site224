51a79e5109505ebf26db2da13ffba0a799b7bef2
audgks5551 <audgks5551@daum.net>
2021-12-23 13:41:13 +0900

    정리2

--- questions/urls.py
+++ questions/urls.py
@@ -6,6 +6,8 @@ app_name = "questions"
 urlpatterns = [
     path('', views.showQuestion, name="show"),
     path('create/', views.createQuestion, name="create"),
-    path('<int:question_count>/modify', views.modifyQuestion, name="modify"),
-    path('<int:question_count>/delete', views.deleteQuestion, name="delete"),
+    path('<int:question_count>/', include([
+        path('modify/', views.modifyQuestion, name="modify"),
+        path('delete/', views.deleteQuestion, name="delete"),
+    ])),
 ]
