ad67b25e5b850a0f5c39e99c32eb7eb84e16f8be
audgks5551 <audgks5551@daum.net>
2021-12-23 23:32:22 +0900

    상단바, 하단바 꾸미기

--- questions/views.py
+++ questions/views.py
@@ -14,30 +14,34 @@ def createQuestion(request, product_id):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.user = User.objects.last()
             question.object_id = product_id
-            user = User.objects.last() 
-            question.user = user
             question.content_type = ContentType.objects.get(model='Question')
             question.save()
             return redirect('products:detail', product_id)
     else:
         form = QuestionForm()
 
-    form = QuestionForm()
     context = {'form': form, 'product': product}
     return render(request, 'question_create.html', context)
 
 # 질문 보기
+
+
 def showQuestion(request, product_id):
 
-    question_list = Question.objects.filter(object_id=product_id, user=User.objects.last())
+    question_list = Question.objects.filter(
+        object_id=product_id, user=User.objects.last())
     context = {'question_list': question_list, 'product_id': product_id}
     return render(request, 'question_list.html', context)
 
 # 질문 수정
+
+
 def modifyQuestion(request, product_id, question_count):
 
-    question_list = Question.objects.filter(object_id=product_id, user=User.objects.last())
+    question_list = Question.objects.filter(
+        object_id=product_id, user=User.objects.last())
     question = question_list[question_count-1]
 
     if request.method == "POST":
@@ -49,14 +53,17 @@ def modifyQuestion(request, product_id, question_count):
     else:
         form = QuestionForm(instance=question)
 
-    context = {'question_list': question_list, 'product_id': product_id, 'form': form, 'question_count': question_count}
+    context = {'question_list': question_list, 'product_id': product_id,
+               'form': form, 'question_count': question_count}
     return render(request, 'question_list.html', context)
 
 # 질문 삭제
+
+
 def deleteQuestion(request, product_id, question_count):
-    question_list = Question.objects.filter(object_id=product_id, user=User.objects.last())
+    question_list = Question.objects.filter(
+        object_id=product_id, user=User.objects.last())
     question = question_list[question_count-1]
     question.delete()
 
     return redirect('products:questions:show', product_id)
-
